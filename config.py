d78c0d7d9f1ed3e696d8206181b4e9e23e8554b4
THUNDER <143776436+BRANDED-THUNDER@users.noreply.github.com>
2023-12-25 20:45:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -34,7 +34,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/BRANDED-THUNDER",
+    "https://github.com/BRANDED-THUNDER/THUNDERXMusic",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
